2c7f45a4fb07592c669e9ef8a7ea0e4d2977ffe1
Wonhee Jang <Qtsky89@gmail.com>
2025-03-30 23:34:08 +0900

    #6.4 Cleanup (13:20) done

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,31 +1,51 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  console.log("render");
-
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("created");
+    return () => console.log("destroyed");
   }, []);
 
-  const [keyword, setKeyword] = useState("");
-  useEffect(() => {
-    console.log(`search for ${keyword}`);
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+
   return (
     <>
-      <input
-        value={keyword}
-        onChange={(event) => setKeyword(event.target.value)}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={() => setShowing((current) => !current)}>click</button>
     </>
   );
 }
+
+// import Button from "./Button";
+// import styles from "./App.module.css";
+// function App() {
+//   const [count, setCount] = useState(0);
+//   console.log("render");
+
+//   useEffect(() => {
+//     console.log("I run only once");
+//   }, []);
+
+//   const [keyword, setKeyword] = useState("");
+//   useEffect(() => {
+//     console.log(`search for ${keyword}`);
+//   }, [keyword]);
+//   return (
+//     <>
+//       <input
+//         value={keyword}
+//         onChange={(event) => setKeyword(event.target.value)}
+//         type="text"
+//         placeholder="Search here"
+//       />
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount((prev) => prev + 1)}>click me</button>
+//     </>
+//   );
+// }
 
 export default App;
