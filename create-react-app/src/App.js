1fd30ec2194763a3158013b52e1b9eeb91b4b244
Wonhee Jang <Qtsky89@gmail.com>
2025-03-30 20:23:33 +0900

    #5.1 Tour of CRA (12:40) done

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,7 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <>
-      <h1>Wecome back!</h1>
+      <h1 className={styles.title}>Wecome back!</h1>
+
+      <Button text="test"></Button>
     </>
   );
 }
