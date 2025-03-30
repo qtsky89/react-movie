c6f5133f2bf4fda2ed791ce748591254c4a71a3d
Wonhee Jang <Qtsky89@gmail.com>
2025-03-30 20:51:14 +0900

    #6.0 Introduction (05:51) done

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,12 +1,15 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  console.log("render");
+
   return (
     <>
-      <h1 className={styles.title}>Wecome back!</h1>
-
-      <Button text="test"></Button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>click me</button>
     </>
   );
 }
