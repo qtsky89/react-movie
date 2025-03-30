513344f53a6b4125fdae3fcd4b1eee7479f83056
Wonhee Jang <Qtsky89@gmail.com>
2025-03-30 22:30:02 +0900

    Learned useEffect

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,13 +1,27 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   console.log("render");
 
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  const [keyword, setKeyword] = useState("");
+  useEffect(() => {
+    console.log(`search for ${keyword}`);
+  }, [keyword]);
   return (
     <>
+      <input
+        value={keyword}
+        onChange={(event) => setKeyword(event.target.value)}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{count}</h1>
       <button onClick={() => setCount((prev) => prev + 1)}>click me</button>
     </>
