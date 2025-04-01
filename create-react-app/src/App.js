64557798d74b56855c4275c5f2fc6c46922dd9b1
Wonhee Jang <Qtsky89@gmail.com>
2025-04-01 10:52:18 +0900

    fetching the data from api

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await (
+        await fetch(
+          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+        )
+      ).json();
+
+      setMovies(res.data.movies);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1>Movies {movies.length}</h1>
+      {loading ? <h1>Loading ...</h1> : null}
+
+      <ul>
+        {movies.map((m) => (
+          <li>{m.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
