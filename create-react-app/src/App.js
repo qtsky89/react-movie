ad9e73a9f1b22a343f983c8955091b974d1fe251
Wonhee Jang <Qtsky89@gmail.com>
2025-04-01 11:55:07 +0900

    #7.4 Movie App part Two (10:58) done

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,37 +1,3 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
-
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const res = await (
-        await fetch(
-          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-        )
-      ).json();
-
-      setMovies(res.data.movies);
-      setLoading(false);
-    })();
-  }, []);
-
-  return (
-    <>
-      <h1>Movies {movies.length}</h1>
-      {loading ? <h1>Loading ...</h1> : null}
-
-      {movies.map((m) => (
-        <Movie
-          key={m.id}
-          title={m.title}
-          coverImage={m.medium_cover_image}
-          genres={m.genres}
-          summary={m.summary}
-        />
-      ))}
-    </>
-  );
+  return null;
 }
---
+++ create-react-app/src/routes/Home.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await (
+        await fetch(
+          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+        )
+      ).json();
+
+      setMovies(res.data.movies);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1>Movies {movies.length}</h1>
+      {loading ? <h1>Loading ...</h1> : null}
+
+      {movies.map((m) => (
+        <Movie
+          key={m.id}
+          title={m.title}
+          coverImage={m.medium_cover_image}
+          genres={m.genres}
+          summary={m.summary}
+        />
+      ))}
+    </>
+  );
+}
